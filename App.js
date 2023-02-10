553f88069261f1c9bd66fa1c710424191f427ab0
Muhammad Abdullah <mabdullah2037@outlook.com>
2023-02-10 16:36:49 +0500

    project done

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import Login from './Screens/login.js'
+import Singup from './Screens/singup.js'
 
 const App = () => {
     return (
-        <Login />
+        // <Login />
+        <Singup/>
     )
 }
 
--- Screens/singup.js
+++ Screens/singup.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import { SafeAreaView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 
-const Login = () => {
+const Singup = () => {
     const [email, onChangeEmail] = React.useState('');
     const [password, onChangePassword] = React.useState('');
+    const [name, onChangeName] = React.useState('');
+    const [contact, onChangeContact] = React.useState('');
 
     return (
         <SafeAreaView style={styles.view}>
             <View style={{ alignItems: "center", paddingBottom: 10 }}>
-                <Text style={styles.text}>Login</Text>
+                <Text style={styles.text}>Singup User</Text>
             </View>
+            <TextInput
+                style={styles.input}
+                onChangeText={onChangeName}
+                value={name}
+                placeholder="Full Name"
+                placeholderTextColor="grey"
+            />
+
             <TextInput
                 style={styles.input}
                 onChangeText={onChangeEmail}
@@ -20,16 +30,24 @@ const Login = () => {
             />
             <TextInput
                 style={styles.input}
+                onChangeText={onChangeContact}
+                value={contact}
+                keyboardType="numeric"
+                placeholder="Contact Number"
+                placeholderTextColor="grey"
+            />
+            <TextInput
+                style={styles.input}
                 onChangeText={onChangePassword}
                 value={password}
                 placeholder="Password"
                 placeholderTextColor="grey"
-
             />
-            <View style={{alignItems:"center" , paddingTop:20}}>
-            <TouchableOpacity style={{ backgroundColor: "black", width: "40%", padding: 8 , borderRadius:20 }}>
-                <Text style={{ fontSize: 20, textAlign: "center" }}>Submit</Text>
-            </TouchableOpacity>
+
+            <View style={{ alignItems: "center", paddingTop: 20 }}>
+                <TouchableOpacity style={{ backgroundColor: "black", width: "40%", padding: 8, borderRadius: 20 }}>
+                    <Text style={{ fontSize: 20, textAlign: "center" }}>Submit</Text>
+                </TouchableOpacity>
             </View>
         </SafeAreaView>
 
@@ -42,8 +60,9 @@ const styles = StyleSheet.create({
         margin: 12,
         borderWidth: 1,
         padding: 10,
-        color: "black",
         borderRadius:10,
+        backgroundColor:'white'
+        // color: "black"
     },
     view: {
         backgroundColor: "white",
@@ -57,4 +76,4 @@ const styles = StyleSheet.create({
 
 });
 
-export default Login;
+export default Singup;
